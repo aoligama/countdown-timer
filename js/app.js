ffab47c1d5fbc70019308e9f2a1113d5b813eb0e
Amanda Gama <amgama17@gmail.com>
2020-04-29 14:40:28 -0300

    update style.css, index.html and app.js
    New functions, fireworks and new structure

--- js/app.js
+++ js/app.js
@@ -1,52 +1,69 @@
-loadEventListeners();
+//loadEventListeners();
+//
+//function loadEventListeners() {
+//	document.addEventListener('DOMContentLoaded', function() { calcTime(''); });
+//};
 
-function loadEventListeners() {
-	document.addEventListener('DOMContentLoaded', function() { calcTime(); });
-};
-
-var timeTo = document.getElementById('time-to').value,
-		date,
+var date,
 		now = new Date(),
 		newYear = new Date('1.1.2020').getTime(),
 		startTimer = '';
 
 function calcTime(dates) {
-	//ui variables
-	clearInterval(startTimer);
+	dates = $('#time-to').val();
+	if( typeof(dates) != 'undefined' && dates != ''){
 
-	if(typeof(dates) == 'undefined'){
-		date = new Date(newYear).getTime();
-	}else {
-		date = new Date(dates).getTime();
-	}
+		clearInterval(startTimer);
 
-	function updateTimer(date){
-
-		var now = new Date().getTime();
-		var distance = date - now;
-
-		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-		$('#days').val(days);
-		$('#hours').val(hours);
-		$('#minutes').val(minutes);
-		$('#seconds').val(seconds);
-
-        
-		if(now >= date){
-			clearInterval(startTimer);
-			$('#days').val('D');
-			$('#hours').val('O');
-			$('#minutes').val('N');
-			$('#seconds').val('E');
+		if(typeof(dates) == 'undefined'){
+			date = new Date(newYear).getTime();
+		}else {
+			date = new Date(dates).getTime();
 		}
+
+		function updateTimer(date){
+
+			var now = new Date().getTime();
+			var distance = date - now;
+
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+			$('#days').val(days);
+			$('#hours').val(hours);
+			$('#minutes').val(minutes);
+			$('#seconds').val(seconds);
+		
+			if(now >= date){
+				clearInterval(startTimer);
+				$('#days').val('D');
+				$('#hours').val('O');
+				$('#minutes').val('N');
+				$('#seconds').val('E');
+				$('.pyro').removeClass('hidden');
+				$('.input-time').css({
+					'background-color': '#26c281',
+					'color': '#000',
+				});
+			}
+		}
+
+		startTimer = setInterval(function() { updateTimer(date); }, 1000);
+	} else {
+		bootbox.alert('Try to put some valid date next time :)')
+		
+		$('#days').val('');
+		$('#hours').val('');
+		$('#minutes').val('');
+		$('#seconds').val('');
 	}
-
-	startTimer = setInterval(function() { updateTimer(date); }, 1000);
-
 }
 
+function stopFireworks(){
+	$('.pyro').addClass('hidden');
+	
+
+
+}
